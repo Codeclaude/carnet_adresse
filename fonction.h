78cfae2b2de61538e45cf7d4e07ee92af70666ce
Codeclaude <claudeck2007@gmail.com>
2025-01-27 17:30:32 +0100

    Premier commit pour carnet_adresse

---
+++ fonction.h
@@ -0,0 +1,13 @@
+#ifndef FONCTION_H
+#define FONCTION_H
+struct Contact {
+    char nom[20];
+    char prenom[20];
+    char email[20];
+    char telephone[10];
+};
+void affichercontact(const Contact& contact);
+void afficherlescontacts(Contact contacts[], int taille);
+void ajoutercontact(Contact contacts[], int& taille, int capacite);
+ void affichermenu();
+ #endif
